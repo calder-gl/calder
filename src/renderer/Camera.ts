6cbadfa1b0b639eebef5648d38e29518142c34ae
Dave Pagurek <dave@davepagurek.com>
2018-05-02 12:17:20 -0400

    Update comments

--- src/renderer/Camera.ts
+++ src/renderer/Camera.ts
@@ -1,8 +1,8 @@
 import { mat4, quat, vec3 } from 'gl-matrix';
 
 /**
- * Represents the orientation of the camera in a scene, defined by a position for the camera and a
- * target for the camera to look at
+ * This represents the orientation of the camera in a scene, defined by a position for the camera and
+ * a target for the camera to look at.
  */
 export class Camera {
     private static readonly up: vec3 = vec3.fromValues(0, 1, 0);
@@ -10,11 +10,14 @@ export class Camera {
 
     private position: vec3 = vec3.fromValues(0, 0, 0);
     private target: vec3 = vec3.copy(vec3.create(), Camera.defaultDirection);
+
+    // `transform` is only updated when getTransform is called. The `dirty` flag is used to signal
+    // that an update has occurred and the cached transform value needs to be recomputed.
     private transform: mat4 = mat4.create();
     private dirty: boolean = true;
 
     /**
-     * @param {vec3} position The world-space coordinate to move the camera to, preserving rotation
+     * @param {vec3} position The world-space coordinate to move the camera to, preserving rotation.
      */
     public moveTo(position: vec3) {
         const direction = vec3.subtract(vec3.create(), position, this.position);
@@ -25,7 +28,7 @@ export class Camera {
 
     /**
      * @param {vec3} position The world-space coordinate to move the camera to, keeping the camera
-     * rotated towards its previous target
+     * rotated towards its previous target.
      */
     public moveToWithFixedTarget(position: vec3) {
         vec3.copy(this.position, position);
@@ -34,7 +37,7 @@ export class Camera {
 
     /**
      * @param {vec3} direction A world-space direction vector that will be added to the camera's
-     * position, preserving its existing rotation
+     * position, preserving its existing rotation.
      */
     public moveBy(direction: vec3) {
         vec3.add(this.position, this.position, direction);
@@ -44,7 +47,7 @@ export class Camera {
 
     /**
      * @param {vec3} direction A world-space direction vector that will be added to the camera's
-     * position, keeping the camera pointed towards its previous target
+     * position, keeping the camera pointed towards its previous target.
      */
     public moveByWithFixedTarget(direction: vec3) {
         vec3.add(this.position, this.position, direction);
@@ -52,7 +55,7 @@ export class Camera {
     }
 
     /**
-     * @param {vec3} target A world-space coordinate that the camera will rotate to face
+     * @param {vec3} target A world-space coordinate that the camera will rotate to face.
      */
     public lookAt(target: vec3) {
         vec3.copy(this.target, target);
@@ -82,7 +85,7 @@ export class Camera {
 
     /**
      * Using the current position and target of the camera, produces a transformation matrix to
-     * bring world space coordinates into camera space
+     * bring world space coordinates into camera space.
      */
     public getTransform(): mat4 {
         if (this.dirty) {

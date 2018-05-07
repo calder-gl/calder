155d0227025743617768403644395c6952edddce
Dave Pagurek <dave@davepagurek.com>
2018-05-07 15:35:08 -0400

    Make geometry use classes, add comments

--- src/geometry/WorkingGeometry.ts
+++ src/geometry/WorkingGeometry.ts
@@ -1,13 +1,41 @@
-import { vec3 } from 'gl-matrix';
+import { vec4 } from 'gl-matrix';
 
-export type Face = {
-    // The indices for the vertices representing the three points of this triangle/quad
-    indices: number[];
-    normal: vec3;
-};
+/**
+ * A representation of a surface on an object.
+ */
+export class Face {
+    /**
+     * References indices of vertices in a WorkingGeometry that make up a polygon. This polygon is
+     * the object's surface. This should be of length 3 for a triangle or 4 or a quad.
+     */
+    public indices: number[];
 
-export type WorkingGeometry = {
-    vertices: vec3[];
-    faces: Face[];
-    controlPoints: vec3[];
-};
+    /**
+     * A vector representing the direction out of the front of the face. The normal n should have
+     * n[3] = 1 to ensure that it is a vector in Affine space.
+     */
+    public normal: vec4;
+}
+
+/**
+ * A representation of geometry while modelling is happening.
+ */
+export class WorkingGeometry {
+    /**
+     * The points that make up the geometry, in no particular order. For each vertex v, v[3] must
+     * be equal to 0 to ensure that it is a point in Affine space rather than a vector.
+     */
+    public vertices: vec4[];
+
+    /**
+     * The surfaces of the object. Each face includes the indices of the vertices that make up the
+     * surface's polygon.
+     */
+    public faces: Face[];
+
+    /**
+     * A set of points to snap to or reference when combining geometries. For each point p, p[3]
+     * must be equal to 0 to ensure that it is a point in Affine space.
+     */
+    public controlPoints: vec4[];
+}

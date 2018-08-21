1f68694762accfc1cd860d2f2a25492ad02ee6ab
Dave Pagurek <dave@davepagurek.com>
2018-08-21 10:49:18 -0400

    Add test

---
+++ tests/armature/Model.spec.ts
@@ -0,0 +1,53 @@
+import {
+    Armature,
+    Model,
+    Node,
+    RGBColor,
+    Shape
+} from '../../src/calder';
+
+const bone = Armature.define((root: Node) => {
+    root.createPoint('base', { x: 0, y: 0, z: 0 });
+    root.createPoint('tip', { x: 0, y: 1, z: 0 });
+});
+
+describe('Model', () => {
+    describe('exportOBJ', () => {
+        it('generates the expected file for a simple shape', () => {
+            const model = Model.create(bone());
+            model.add(model.root().point('base').attach(Shape.sphere()));
+            const exported = model.exportOBJ('model', RGBColor.fromHex('#000000'));
+
+            // Assert that the given name was used for the model and material
+            expect(exported.obj).toEqual(expect.stringMatching(new RegExp('^o model', 'm')));
+            expect(exported.obj).toEqual(expect.stringMatching(new RegExp('^mtllib model.mtl', 'm')));
+
+            // Check that there is a vertex normal for every vertex
+            const vertices = exported.obj.match(new RegExp('^v .*$', 'mg'));
+            const normals = exported.obj.match(new RegExp('^vn .*$', 'mg'));
+            expect(vertices).not.toBeNull();
+            expect(normals).not.toBeNull();
+            expect(vertices && vertices.length).toEqual(normals && normals.length);
+
+            const faceRegex = new RegExp('^f (\\d+) (\\d+) (\\d+)$', 'mg');
+
+            // Iterate over each face
+            let match: string[] | null = null;
+            do {
+                match = faceRegex.exec(exported.obj);
+                if (match !== null) {
+                    for (let i = 1; i <= 3; i += 1) {
+                        // Check that faces all refer to real vertices
+                        const vertex = parseInt(match[i], 10);
+                        expect(vertex).toBeGreaterThan(0);
+                        expect(vertex).toBeLessThanOrEqual(vertices ? vertices.length : 0);
+                    }
+                }
+            } while (match);
+
+            // Check that a material was produced
+            expect(exported.mtl).toEqual(expect.stringMatching(new RegExp('^newmtl material0$', 'm')));
+            expect(exported.mtl).toEqual(expect.stringMatching(new RegExp('^newmtl material0$', 'm')));
+        });
+    });
+});

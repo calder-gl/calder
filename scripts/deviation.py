30f1ccd9283229585c51848167da7b82c16d2b85
Dave Pagurek <dave@davepagurek.com>
2018-09-11 21:58:30 -0700

    Graph standard deviation

--- scripts/deviation.py
+++ scripts/deviation.py
@@ -9,14 +9,14 @@ with open(sys.argv[1], 'r') as myfile:
     data = myfile.read()
     generations = [ match.groups()[0].split(', ') for match in re.compile(r'Generation \(\d+\) costs: ((?:[\-\d.]+(?:, )?)+)<br><br>').finditer(data) ]
     costs = [ [ float(cost) for cost in generationCostString ] for generationCostString in generations ]
+    stdDevs = [ np.std(values) for values in costs ]
 
     x = []
     y = []
 
-    for idx, generation in enumerate(costs):
-        for cost in generation:
-            x.append(idx)
-            y.append(cost)
+    for idx, stdDev in enumerate(stdDevs):
+        x.append(idx)
+        y.append(stdDev)
 
     plt.scatter(x, y)
     plt.show()
